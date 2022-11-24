6e38d783b2ae9e7fddb9cb45bd0a00134c704210
Marie Baude <33251784+MarieBaude@users.noreply.github.com>
2022-11-24 14:28:28 +0100

    init

---
+++ src/main/java/com/simplon/java/Chronometre.java
@@ -0,0 +1,23 @@
+package com.simplon.java;
+
+public class Chronometre {
+	private double time;
+	
+	Chronometre(double time) {
+		this.time = time;
+	}
+	
+	public void advanceTime() {
+		
+	}
+	
+	public void backTime() {
+		
+	}
+	
+	public void date() {
+		
+	}
+	
+	
+}
---
+++ src/main/java/com/simplon/java/exo1.java
@@ -0,0 +1,10 @@
+package com.simplon.java;
+
+public class exo1 {
+
+	public static void main(String[] args) {
+		//voir class chrometre
+
+	}
+
+}
